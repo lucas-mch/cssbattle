c4ac5778f408c788752c99f9f96fc38301f058f0
Lucas Machado de Souza <102778991+lucas-mch@users.noreply.github.com>
2024-04-16 04:06:03 -0300

    This commit will be automatically published

---
+++ README.md
@@ -0,0 +1,20 @@
+# Desafios do CSS Battle
+
+Este repositório contém uma coleção de desafios do [CSS Battle](https://cssbattle.dev/), uma plataforma divertida para praticar e aprimorar suas habilidades em CSS. Cada desafio é apresentado como um arquivo HTML individual, onde você pode visualizar o resultado e a solução.
+
+## Como funciona
+
+1. **Clone o Repositório:** Clone este repositório para o seu ambiente local usando o seguinte comando:
+
+```markdown
+git clone https://github.com/lcs-mch/cssbattle.git
+```
+2. **Instale o Live Server (se ainda não estiver instalado):** Este projeto utiliza o Live Server, uma extensão para o Visual Studio Code que facilita o desenvolvimento web. Se você ainda não tiver instalado, você pode encontrá-lo no [Marketplace do VS Code](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer).
+
+3. **Abra o Projeto:** Abra o projeto no Visual Studio Code.
+
+4. **Inicie o Live Server:** Abra o arquivo `index.html` e clique com o botão direito do mouse. Selecione "Open with Live Server" para iniciar o servidor local. Isso abrirá a página inicial no seu navegador padrão.
+
+5. **Navegue pelos Desafios:** Na página inicial, você verá uma tabela dinâmica com todos os desafios realizados. Cada desafio é um link para uma página HTML individual que exibe o desafio e sua solução em um `<iframe>`.
+
+6. **Explore os Desafios:** Clique nos links dos desafios para visualizar o resultado e a solução. Você pode modificar o código CSS diretamente na página do desafio para experimentar diferentes abordagens e técnicas.
---
+++ assets/script.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var pageSize = 10;
+    var currentPage = 1;
+
+    var desafiosTable = document.getElementById('desafios-table');
+    var desafiosBody = document.getElementById('desafios-body');
+    var pagination = document.getElementById('pagination');
+    var iframe = document.getElementById('iframe');
+
+    function carregarDesafios() {
+
+        var desafios = [
+            { link: `desafios/13-04-2024.html`, data: `cssbattle/desafios/13-04-2024` },
+            { link: `desafios/14-04-2024.html`, data: `cssbattle/desafios/14-04-2024` },
+            { link: `desafios/15-04-2024.html`, data: `cssbattle/desafios/15-04-2024` }
+        ];
+        mostrarDesafios(desafios);
+    }
+
+    function mostrarDesafios(desafios) {
+
+        desafiosBody.innerHTML = '';
+
+
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize, desafios.length);
+
+        var desafiosPagina = desafios.slice(startIndex, endIndex);
+
+        desafiosPagina.forEach(function (desafio) {
+            var row = document.createElement('tr');
+            var dataCell = document.createElement('td');
+            dataCell.textContent = desafio.data;
+            row.appendChild(dataCell);
+            var linkCell = document.createElement('td');
+            var link = document.createElement('a');
+            link.href = '#';
+            link.textContent = desafio.link;
+            link.setAttribute('data-src', desafio.link);
+            linkCell.appendChild(link);
+            row.appendChild(linkCell);
+            desafiosBody.appendChild(row);
+        });
+
+        mostrarPaginacao(desafios.length);
+    }
+
+    function mostrarPaginacao(totalDesafios) {
+        var totalPages = Math.ceil(totalDesafios / pageSize);
+
+        pagination.innerHTML = '';
+
+        for (var i = 1; i <= totalPages; i++) {
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+            a.href = '#';
+            a.textContent = i;
+            a.dataset.page = i;
+            li.appendChild(a);
+            pagination.appendChild(li);
+        }
+
+
+        pagination.addEventListener('click', function (event) {
+            if (event.target.tagName === 'A') {
+                currentPage = parseInt(event.target.dataset.page);
+                carregarDesafios();
+            }
+        });
+    }
+
+    desafiosBody.addEventListener('click', function (event) {
+        var target = event.target;
+        if (target.tagName === 'A') {
+            var src = target.getAttribute('data-src');
+            iframe.src = src;
+        }
+    });
+
+    carregarDesafios();
+});
